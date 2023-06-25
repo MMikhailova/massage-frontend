5f42f264da653e7ca17af8761bd8407e501950b1
MMikhailova <naplesaries2014@gmail.com>
2023-06-25 22:15:51 +0200

    fixed error

--- src/components/Treatment.jsx
+++ src/components/Treatment.jsx
@@ -1,20 +1,12 @@
 import React, { useEffect, useState } from "react";
 
-import { Box, Typography } from "@mui/material";
-import StyledButton from "./Button";
 import { getTreatments } from "../api/viewModels/getTreatments";
 import BasicGrid from "./HomeTreatments";
 import { getGallery } from "../api/viewModels/getGallery";
 import WovenImageList from "./Gallery";
-import DrawerAppBar from "./Navigation";
-import Footer from "./Footer";
 
-const positionBtn = {
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  position: "absolute",
-};
+
+
 const Treatment = ({ page }) => {
   const [treatments, setTreatments] = useState([]);
   const [gallery, setGallery] = useState([]);
