337bf1d4877378720bf471ff5fcb915e9a546c14
MMikhailova <naplesaries2014@gmail.com>
2025-01-06 21:39:49 +0100

    about changed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -89,8 +89,38 @@ height:"100%",
             />
           </Grid>
         </Grid>
-        <Typography py="3rem" variant="h2" component="h1">
-          About Natalya Werthaim
+        <Typography
+          display={{ xs: "block", md: "none" }}
+          align="center"
+          variant="h5"
+          component="h4"
+          color="#DF7861"
+          fontWeight="bold"
+          fontStyle= "italic"
+          py="3rem"
+        >
+          Welcome to BeVibrant - Your Path to Holistic Healing and
+          Transformation
+        </Typography>
+        <Typography
+          display={{ xs: "none", md: "block" }}
+          pt="3rem"
+          variant="h2"
+          component="h1"
+        >
+          {" "}
+          Welcome to BeVibrant
+        </Typography>
+        <Typography
+          display={{ xs: "none", md: "block" }}
+          align="center"
+          pb="3rem"
+          variant="h5"
+          component="h4"
+          color="#94B49F"
+          fontWeight="bold"
+        >
+          Your Path to Holistic Healing and Transformation
         </Typography>
         <Grid
           container
@@ -113,60 +143,138 @@ height:"100%",
           </Grid>
           <Grid xs={12} md={5} item>
             <Typography variant="subtitle1">
-              Originally from Ukraine, I have spent most of my life in Tel Aviv,
-              Israel. I am fluent in{" "}
+              Originally from Ukraine, I grew up in Tel Aviv, Israel, and have
+              spent most of my life living in seven different countries. I
+              eventually settled in Brussels, Belgium, where I’ve been sharing
+              the holistic wisdom and knowledge gained during my travels with
+              dedicated students and clients since 2010. They come to me not
+              only for physical healing and relaxation but for a complete
               <Box component="span" fontStyle="Italic">
-                English, French, Greek, Russian, and Hebrew
-              </Box>
-              , welcoming my clients in these languages to ensure a smooth and
-              inclusive experience for individuals seeking holistic well-being.
-              <br />
-              I embarked on a spiritual journey in my late 20s. This quest for
-              deeper meaning led me to live in seven different countries,
-              explore various spiritual techniques, and learn different bodywork
-              disciplines to enhance well-being. My journey began in Prague,
-              where I ran my own events company. However, I felt unfulfilled by
-              the corporate world and sought something more meaningful in my
-              life. After a long period of traveling, this search eventually led
-              me to India, where I spent almost a year living in an Ashram in
-              Puna. Here, I delved deep into Yoga and meditation, studying at
-              the Iyengar Institute and completing various transformation
-              courses under the tutelage of spiritual leaders. <br />
-              During my time in India, I was lucky to meet the great Master
-              Kusum Modak and study Ayurvedic Yoga massage methods at her
-              institute and to become a certified therapist. I have been
-              passionately dedicated to this practice ever since, using it to
-              help people reconnect with their inner selves and to solve
-              physical and mental issues.From India, my spiritual journey led me
-              to Greece, where I met the famous Reiki master Arjava, and
-              obtained my II degree in Reiki.
+                {" "}
+                holistic experience
+              </Box>{" "}
+              that supports emotional healing, restores balance, and helps them
+              rediscover their life’s path. My clients span all ages, united by
+              a common goal: to live a vibrant and meaningful life.
+            </Typography>
+            <Typography variant="subtitle1">
+              I am fluent in{" "}
+              <Box component="span" fontStyle="Italic">
+                {" "}
+                English, French, Greek, Russian, and Hebrew,
+              </Box>{" "}
+              warmly welcoming clients from diverse backgrounds. With a blend of
+              empathy, deep spiritual wisdom, and an understanding of my
+              clients’ needs, I ensure an inclusive and seamless experience for
+              anyone seeking holistic well-being.
+            </Typography>
+            <Typography variant="subtitle1">
+              I am deeply passionate about my craft and am always evolving. My
+              commitment to growth has led me to compete in{" "}
+              <Box component="span" fontStyle="Italic">
+                {" "}
+                European massage championships,
+              </Box>{" "}
+              where I proudly placed 13th in Europe. Additionally, I am honoured
+              to be a member of the{" "}
+              <Box component="span" fontStyle="Italic">
+                {" "}
+                Belgian National Massage Team representing Ayurveda.
+              </Box>{" "}
+              In 2024 I was awarded as the Top rated therapist in Belgium. This
+              dedication to excellence drives me to continually refine my skills
+              and offer the best possible service to my clients.
             </Typography>
           </Grid>
-
           <Grid xs={12} md={10} item>
             <Typography variant="subtitle1">
-              I also became a certified Yoga teacher studying at the Canadian
-              Sempreviva Yoga School, before opening my first Yoga studio in
-              Kythira, a magical Greek island. Eventually, I moved and settled
-              in Belgium, where I have been sharing my wisdom and knowledge with
-              dedicated students and clients in my studio in Brussels for the
-              past 15 years. Through my work as a therapist and Yoga teacher, I
-              strive to help people achieve greater well-being, inner peace, and
-              physical health.
+              My journey towards holistic healing began in 2005, when I made the
+              life-changing decision to leave my successful career as an event
+              and party organizer in Prague. Seeking a deeper connection with
+              myself and a more meaningful life, I embarked on a path of
+              self-discovery that led me to India. There, I spent a year living
+              as a monk in an ashram (meditation center) in Pune. It was during
+              this time that I deepened my spiritual practice, studied various
+              bodywork disciplines, and learned from world-renowned spiritual
+              masters.
+            </Typography>
+            <Typography variant="subtitle1">
+              <Typography variant="subtitle1">
+                Through my immersion in the ancient wisdom of{" "}
+                <Box component="span" fontStyle="Italic">
+                  {" "}
+                  Ayurveda, yoga, meditation, and holistic healing,
+                </Box>{" "}
+                I experienced profound personal growth and inner healing,
+                ultimately finding peace and happiness. It was during this
+                transformative journey that my mission became clear: to share
+                the depth and beauty of these practices with the Western world
+                in a way that aligns with modern lives.
+              </Typography>
             </Typography>
           </Grid>
+          <Grid xs={12} md={5} item>
+            <Typography variant="subtitle1">
+              With this vision in mind, I created the brand{" "}
+              <Box component="span" fontStyle="Italic">
+                {" "}
+                Be Vibrant,
+              </Box>{" "}
+              combining traditional healing techniques with modern practices. I
+              integrate different{" "}
+              <Box component="span" fontStyle="Italic">
+                {" "}
+                Ayurvedic massage techniques, Yoga discipline, Reiki, Pranayama
+                (breathwork), Sound healing, somatic tools, active meditations
+                for emotional release
+              </Box>{" "}
+              to offer a comprehensive approach to holistic wellness. This
+              unique blend helps clients bridge the gap between physical,
+              mental, and emotional well-being.
+            </Typography>
+            <Typography variant="subtitle1">
+              At{" "}
+              <Box component="span" fontStyle="Italic">
+                {" "}
+                Be Vibrant,
+              </Box>{" "}
+              I offer a safe, nurturing space where you can reconnect with
+              yourself on a profound level, allowing healing and growth to truly
+              happen. After deeply relaxing and healing you physically and
+              mentally, my goal through well-being coaching is to empower you to
+              embrace personal transformation, set life goals, and find deeper
+              meaning in life. The warm oils, powders, and massage tools I use
+              are carefully selected to promote grounding, balance, and
+              rejuvenation, fostering a strong body-mind connection. Whether
+              you're seeking relaxation, healing from emotional trauma, or a
+              deeper connection with yourself, I am here to guide and support
+              you every step of the way.
+            </Typography>
+          </Grid>
+          <Grid xs={12} md={5} item>
+            <img
+              src="../assets/bevibrant_top_rated_massage_therapist.webp"
+              alt="natalya werthaim"
+              style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            />
+          </Grid>
+
           <Grid xs={12} md={10} textAlign="center" item>
             <Typography
-              align="center"
+              align="justify"
               sx={{ xs: 0, md: 2 }}
               variant="h5"
               color="#DF7861"
               fontStyle="italic"
             >
-              “Join me on a transformative journey towards Vibrant well-being,
-              where the power of Yoga, the healing touch of massage and the
-              guidance of a dedicated health coach converge to ignite your inner
-              radiance...”
+              “Be Vibrant is not only a wellness brand; it is a movement
+              dedicated to personal growth, deep healing, and spiritual
+              awakening. If you're looking to transform your life, restore
+              balance, and reconnect with your true self, Be Vibrant offers a
+              complete wellness experience. From different Ayurvedic massage,
+              Yoga, Reiki to transformative Breathwork sessions , I provide a
+              range of healing practices to help you on your journey toward a
+              vibrant and meaningful life.”
             </Typography>
             <Typography
               align="right"
@@ -174,7 +282,7 @@ height:"100%",
               color="#DF7861"
               fontStyle="italic"
             >
-              Natalya W.
+              With Love Natalya Werthaim
             </Typography>
           </Grid>
         </Grid>
